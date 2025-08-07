684acb6fb84e1ef7aeca691f6da7f7607497ecce
SLIMMIND <slimmind@gmail.com>
2025-08-07 09:42:08 +0300

    Plate page, HarvardPlate component

--- components/harvard-plate/HarvardPlate.tsx
+++ components/harvard-plate/HarvardPlate.tsx
@@ -1,3 +1,19 @@
+import Sidebar from '../sidebar';
+import styles from './harvard-plate.module.css';
+
 export const HarvardPlate = () => {
-  return <h1>Harvard Plate Component</h1>;
+	return (
+		<div className={styles.plate}>
+			<Sidebar title='Left Sidebar' mods='left'></Sidebar>
+			{/* <aside
+				className={`${styles.plate__sidebar} ${styles['plate__sidebar--left']}`}
+			>
+				<h3>Left Sidebar</h3>
+			</aside> */}
+			<nav className={styles.plate__switcher}>Switcher</nav>
+			<div className={styles.plate__roundel}>Plate</div>
+			<div className={styles.measurement}>Measurement</div>
+      <Sidebar title='Right Sidebar' mods='right'></Sidebar>
+		</div>
+	);
 };
