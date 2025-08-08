c291375c0ef76d3ee2cb746a46eba9183d2bc2bf
SLIMMIND <slimmind@gmail.com>
2025-08-08 22:39:43 +0300

    Measurement component

--- components/plate-roundel/PlateRoundel.tsx
+++ components/plate-roundel/PlateRoundel.tsx
@@ -1,16 +1,16 @@
 import styles from './plate-roundel.module.css';
 
 export const PlateRoundel = () => {
-	return (
-		<div className={styles.plateRoundel}>
-			<div className={styles.plateSection}>
-				<div className={styles.plateVegetables}></div>
-				<div className={styles.plateFruits}></div>
-			</div>
-			<div className={styles.plateSection}>
-				<div className={styles.plateProteins}></div>
-				<div className={styles.plateCarbohydrates}></div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.plateRoundel}>
+      <div className={styles.plateSection}>
+        <div className={styles.plateVegetables}></div>
+        <div className={styles.plateFruits}></div>
+      </div>
+      <div className={styles.plateSection}>
+        <div className={styles.plateProteins}></div>
+        <div className={styles.plateCarbohydrates}></div>
+      </div>
+    </div>
+  );
 };
